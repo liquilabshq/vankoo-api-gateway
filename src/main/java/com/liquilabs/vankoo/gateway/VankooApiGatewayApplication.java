69722648dce6183b8a9411e056b1b53e84623646
Salim Ramirez <salimramirezm@gmail.com>
2026-03-03 17:42:35 -0500

    chore: primer commit

---
+++ src/main/java/com/liquilabs/vankoo/gateway/VankooApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.liquilabs.vankoo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VankooApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VankooApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liquilabs/vankoo/gateway/VankooApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquilabs.vankoo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VankooApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
